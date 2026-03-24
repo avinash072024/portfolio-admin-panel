41d4dd51f5c176bc656daf456ffaf5c5fcaf46d1
Avinash <avinashmarbhal1994@gmail.com>
2026-03-24 14:33:17 +0530

    again api url changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5000/api'
+    // apiUrl: 'http://localhost:5000/api'
+    apiUrl: 'https://portfolio-backend-75va.onrender.com/api'
 };
