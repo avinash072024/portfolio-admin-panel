9c44c2a5bdc40096b636a47c11a139ae8707caa2
Avinash <avinashmarbhal1994@gmail.com>
2026-04-02 14:53:20 +0530

    some changes

--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -1,6 +1,6 @@
 export class Constants {
     public static APP_NAME1: string = 'Admin';
-    public static APP_NAME2: string = 'Panel';
+    public static APP_NAME2: string = 'Portal';
     public static APP_NAME_COMBINE: string = this.APP_NAME1 + '-' + this.APP_NAME2;
     public static APP_NAME: string = this.APP_NAME1 + ' ' + this.APP_NAME2;
     public static THEME_KEY: string = (this.APP_NAME_COMBINE + '-' + 'theme').toLowerCase();
