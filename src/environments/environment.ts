542c6aad338f4edd7f9ec941a726ec43da5b129d
avinash072024 <www.avi.marbhal@gmail.com>
2026-09-19 00:42:12 +0530

    API Url changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     // apiUrl: 'http://localhost:5000/api'
-    apiUrl: 'https://portfolio-backend-75va.onrender.com/api'
+    apiUrl: 'https://portfolio-backend-skax.onrender.com/api'
 };
